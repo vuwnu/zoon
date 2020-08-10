424aae6a159e594bb8818557cccd4e8f5148ea74
viri.space <admin@viri.xyz>
2020-08-10 13:48:22 +0100

    updated navbar element. set current page is now seperate function

--- JS/zoon.js
+++ JS/zoon.js
@@ -192,13 +192,15 @@ class ZoonVariable extends HTMLElement {
 
 class ZoonNavbar extends HTMLElement {
   connectedCallback() {
-    // Sets active page in navbar
+    this.setCurrentPage();
+  }
+  setCurrentPage() {
     let current_location = location.pathname;
     if (current_location === "/") return;
     let nav_items = this.getElementsByTagName("a");
     for (let i = 0, len = nav_items.length; i < len; i++) {
       if (nav_items[i].getAttribute("href").indexOf(current_location) !== -1) {
-        nav_items[i].className = "currentPage";
+        nav_items[i].className = "current-page";
       }
     }
   }
