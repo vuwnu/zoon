5eb3f0626c52b33476e23a076cc5e0c1300153b0
viri.space <admin@viri.xyz>
2020-09-13 19:55:18 +0100

    added defaults to z-title attributes

--- src/zoon.js
+++ src/zoon.js
@@ -124,9 +124,9 @@ class ZoonHTML extends HTMLElement {
 
 class ZoonTitle extends HTMLElement {
   run() {
-    let main = this.getAttribute('title')
-    let prepend = this.getAttribute('prepend')
-    let append = this.getAttribute('append')
+    let main = this.getAttribute('title') || ""
+    let prepend = this.getAttribute('prepend') || ""
+    let append = this.getAttribute('append') || ""
 
     let titleFull = `${prepend}${main}${append}`
 
