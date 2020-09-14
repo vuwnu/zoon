51a869797a3778a61994dfd96fce93bba73fb51a
viri.space <admin@viri.xyz>
2020-09-14 19:54:25 +0100

    updated z-table element

--- src/zoon.js
+++ src/zoon.js
@@ -311,14 +311,40 @@ class ZoonInsert extends HTMLElement {
 
 class ZoonTable extends HTMLElement {
   connectedCallback() {
-    let source = this.getAttribute('src');
+    let json = this.getAttribute('src');
 
-    fetch(source)
-      .then(response => response.text())
-      .then(text => {
-        this.innerHTML = text
-      });
+    let cols = Object.keys(json[0]);
 
+    //Map over columns, make headers,join into string
+    let headerRow = cols
+      .map(col => `<th>${col}</th>`)
+      .join("");
+
+    //map over array of json objs, for each row(obj) map over column values,
+    //and return a td with the value of that object for its column
+    //take that array of tds and join them
+    //then return a row of the tds
+    //finally join all the rows together
+    let rows = json
+      .map(row => {
+        let tds = cols.map(col => `<td>${row[col]}</td>`).join("");
+        return `<tr>${tds}</tr>`;
+      })
+      .join("");
+
+    //build the table
+    const table = `
+  	<table>
+  		<thead>
+  			<tr>${headerRow}</tr>
+  		<thead>
+  		<tbody>
+  			${rows}
+  		<tbody>
+  	<table>`;
+
+    return table;
+    this.innerHTML = table;
     zoon.log('z-table', '#de2f7d', `Built table`);
   }
 }
