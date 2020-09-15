21435164cd4489e3beace432d0ab3450957fbaac
viri.space <admin@viri.xyz>
2020-09-15 14:18:09 +0100

    updated z-table element

--- src/zoon.js
+++ src/zoon.js
@@ -310,15 +310,15 @@ class ZoonInsert extends HTMLElement {
 }
 
 class ZoonTable extends HTMLElement {
-  connectedCallback() {
+  render() {
     let json = window[this.getAttribute('src')]
 
     let cols = Object.keys(json[0]);
 
     //Map over columns, make headers,join into string
     let headerRow = cols
-      .map(col => `<th>${col}</th>`)
-      .join("");
+    .map(col => `<th>${col}</th>`)
+    .join("");
 
     //map over array of json objs, for each row(obj) map over column values,
     //and return a td with the value of that object for its column
@@ -326,26 +326,31 @@ class ZoonTable extends HTMLElement {
     //then return a row of the tds
     //finally join all the rows together
     let rows = json
-      .map(row => {
-        let tds = cols.map(col => `<td>${row[col]}</td>`).join("");
-        return `<tr>${tds}</tr>`;
-      })
-      .join("");
+    .map(row => {
+      let tds = cols.map(col => `<td>${row[col]}</td>`).join("");
+      return `<tr>${tds}</tr>`;
+    })
+    .join("");
 
     //build the table
     const table = `
-  	<table>
-  		<thead>
-  			<tr>${headerRow}</tr>
-  		<thead>
-  		<tbody>
-  			${rows}
-  		<tbody>
-  	<table>`;
+    <table>
+      <thead>
+        <tr>${headerRow}</tr>
+      <thead>
+      <tbody>
+        ${rows}
+      <tbody>
+    <table>`;
 
-    return table;
     this.innerHTML = table;
     zoon.log('z-table', '#de2f7d', `Built table`);
+  }
+  connectedCallback() {
+    if (!this.rendered) {
+      this.render();
+      this.rendered = true;
+    }
   }
 }
 
