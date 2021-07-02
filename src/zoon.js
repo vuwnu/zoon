466cee8935ddcd0bb76fcc03d9da3ff285c8b7bd
viri <43017575+v1r1@users.noreply.github.com>
2021-07-02 19:45:06 +0100

    updated zoon.js

--- src/zoon.js
+++ src/zoon.js
@@ -1,3 +1,25 @@
+let z = {
+    update(id, path) {
+        let entity = $q('#pageSelect').value;
+        $q(id).setAttribute('src', path + entity);
+    },
+    loadPage(page) {
+        $q('z-html').setAttribute('src', page);
+    }
+};
+let zoon = {
+    log(element, color, text) {
+        const font_size = 'font-size:15px;';
+        const logSTYLE = `color:#888;${font_size}`;
+        const tagSTYLE = `color:#ab4cef;${font_size}`;
+        const elemSTYLE = `color:${color};${font_size}`;
+        const zoonTag = `%c[zoon] %c[${element}] %c%s`;
+        console.log(zoonTag, tagSTYLE, elemSTYLE, logSTYLE, text);
+    }
+};
+let zdata;
+const $q = document.querySelector.bind(document);
+const $qa = (css, parent = document) => Array.from(parent.querySelectorAll(css));
 class ZoonFrame extends HTMLElement {
     constructor() {
         super();
@@ -450,25 +472,3 @@ window.addEventListener('load', (event) => {
         defineElements();
     }, 100);
 });
-let z = {
-    update(id, path) {
-        let entity = $q('#pageSelect').value;
-        $q(id).setAttribute('src', path + entity);
-    },
-    loadPage(page) {
-        $q('z-html').setAttribute('src', page);
-    }
-};
-let zoon = {
-    log(element, color, text) {
-        const font_size = 'font-size:15px;';
-        const logSTYLE = `color:#888;${font_size}`;
-        const tagSTYLE = `color:#ab4cef;${font_size}`;
-        const elemSTYLE = `color:${color};${font_size}`;
-        const zoonTag = `%c[zoon] %c[${element}] %c%s`;
-        console.log(zoonTag, tagSTYLE, elemSTYLE, logSTYLE, text);
-    }
-};
-let zdata;
-const $q = document.querySelector.bind(document);
-const $qa = (css, parent = document) => Array.from(parent.querySelectorAll(css));
